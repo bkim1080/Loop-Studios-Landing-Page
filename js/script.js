a5e8a960c5b1ccc71d08764fedbafbe154c72a17
Bryan Kim <bkim1080@gmail.com>
2022-01-20 16:07:23 -0500

    Build out middle sections. Optimize for mobile.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
 const toggleBtn = document.querySelector('.toggle-btn')
 const navbarLinks = document.querySelector('.navbar-links')
-const header = document.querySelector('.header')
+const headerContents = document.querySelector('.header-contents')
 const mainHeading = document.querySelector('.main-heading')
-const displayNoneSection = document.querySelector('.display-none')
+const nonHeaderSection = document.querySelector('.non-header')
+const headerImg = document.querySelector('.header-img')
 
 toggleBtn.addEventListener('click', () => {
   navbarLinks.classList.toggle('active');  
-  header.classList.toggle('active');
+  headerContents.classList.toggle('active');
   mainHeading.classList.toggle('active');
-  displayNoneSection.classList.toggle('active');
+  nonHeaderSection.classList.toggle('active');
   toggleBtn.classList.toggle('active');
+  headerImg.classList.toggle('active');
 })
