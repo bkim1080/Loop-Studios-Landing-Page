e0c60eca4f06e7236c1f425b8111538e9eafa3f4
Bryan Kim <bkim1080@gmail.com>
2022-01-24 23:54:14 -0500

    Begin to optimize header for desktop

--- js/script.js
+++ js/script.js
@@ -1,15 +1,13 @@
-const toggleBtn = document.querySelector('.toggle-btn')
-const navbarLinks = document.querySelector('.navbar-links')
-const headerContents = document.querySelector('.header-contents')
-const mainHeading = document.querySelector('.main-heading')
-const nonHeaderSection = document.querySelector('.non-header')
-const headerImg = document.querySelector('.header-img')
+const toggleBtn = document.querySelector(".toggle-btn");
+const navbarLinks = document.querySelector(".navbar-links");
+const headerContents = document.querySelector(".header-contents");
+const mainHeading = document.querySelector(".main-heading");
+const nonHeaderSection = document.querySelector(".non-header");
 
-toggleBtn.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active');  
-  headerContents.classList.toggle('active');
-  mainHeading.classList.toggle('active');
-  nonHeaderSection.classList.toggle('active');
-  toggleBtn.classList.toggle('active');
-  headerImg.classList.toggle('active');
-})
+toggleBtn.addEventListener("click", () => {
+	navbarLinks.classList.toggle("active");
+	headerContents.classList.toggle("active");
+	mainHeading.classList.toggle("active");
+	nonHeaderSection.classList.toggle("active");
+	toggleBtn.classList.toggle("active");
+});
